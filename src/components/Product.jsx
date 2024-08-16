a100acb972539333d0c849edf9c0be5f0b4ecfd8
Bharat Kushwah <bharatkushwah407@gmail.com>
2024-08-16 13:27:09 +0530

    done

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -18,7 +18,7 @@ const Product = ({ product }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-between bg-white hover:scale-110 transition-all duration-300  ease-in gap-3 p-4 mt-10 ml-5 rounded-xl shadow-[rgba(0,_0,_0,_0.44)_0px_3px_8px] hover:shadow-[0px_0px_95px_53px_#00000024] ">
+    <div className="flex flex-col items-center justify-between bg-white hover:scale-110 transition-all duration-300 ease-in gap-3 p-4 w-[90%] mt-10 ml-3 rounded-xl shadow-[rgba(0,_0,_0,_0.44)_0px_3px_8px] hover:shadow-[0px_0px_95px_53px_#00000024]">
       <div>
         <h1 className="text-gray-700 font-semibold text-lg text-left truncate mt-1 w-40">
           {product.title}
